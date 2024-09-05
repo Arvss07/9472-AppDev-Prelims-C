76dfbf5961ba049fc8c4d55be8bce32ee5671846
Eugene Kyle Patano <2233071@slu.edu.ph>
2024-09-05 23:49:00 +0800

    Commented out totalCitizen counter

--- citizen/citizen.c
+++ citizen/citizen.c
@@ -85,7 +85,7 @@ ResponseCode addFirst(List *list, Citizen citizen) {
     if (list->head == NULL) {
         list->tail = newNode;
     }
-    *totalCitizen++;
+    // *totalCitizen++;
     list->head = newNode;
     saveListToFile(FILENAME, list);
     return Success;
@@ -107,7 +107,7 @@ ResponseCode addLast(List *list, Citizen citizen) {
         list->tail->next = newNode;
         list->tail = newNode;
     }
-    *totalCitizen++;
+    // *totalCitizen++;
     saveListToFile(FILENAME, list);
     return Success;
 }
@@ -192,7 +192,7 @@ ResponseCode removeCitizen(List *list, int citizenId) {
             list->tail = previous;
         }
     }
-    *totalCitizen--;
+    // *totalCitizen--;
     free(current);
     return Success;
 }
--- citizen/citizen.h
+++ citizen/citizen.h
@@ -4,8 +4,8 @@
 #define FILENAME "./data.csv"
 #define FILEPATH "./"
 
-int c = 0;
-int *totalCitizen = &c;
+// int c = 0;
+// int *totalCitizen = &c;
 
 typedef enum {
     FirstName,
