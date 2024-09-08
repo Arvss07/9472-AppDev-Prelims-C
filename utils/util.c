8a246123f31403f964bd5a7abb0f9dd254d91c5f
ALFRED CHRISTIAN EMMANUEL NGAOSI <2232837@slu.edu.ph>
2024-09-08 21:45:53 +0800

    added methods in util

--- utils/util.c
+++ utils/util.c
@@ -1,5 +1,27 @@
-//
-// Created by Ace Ngaosi on 9/8/24.
-//
-
 #include "util.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../citizen/citizen.h"
+
+Gender stringToGender(const char *str) {
+    if (strcmp(str, "Male") == 0) return MALE;
+    if (strcmp(str, "Female") == 0) return FEMALE;
+    if (strcmp(str, "Others") == 0) return OTHERS;
+    return MALE;
+}
+
+MaritalStatus stringToMaritalStatus(const char *str) {
+    if (strcmp(str, "Single") == 0) return SINGLE;
+    if (strcmp(str, "Married") == 0) return MARRIED;
+    if (strcmp(str, "Divorced") == 0) return DIVORCED;
+    if (strcmp(str, "Widowed") == 0) return WIDOWED;
+    return SINGLE;
+}
+
+ResponseCode stringToResponseCode(const char *str){
+    if (strcmp(str, "Success") == 0) return Success;
+    if (strcmp(str, "Failed") == 0) return Failed;
+    if (strcmp(str, "Not Found") == 0) return NotFound;
+    return Success;
+}
--- utils/util.h
+++ utils/util.h
@@ -1,8 +1,10 @@
-//
-// Created by Ace Ngaosi on 9/8/24.
-//
-
 #ifndef UTIL_H
 #define UTIL_H
+
+#include "../citizen/citizen.h"
+
+Gender stringToGender(const char *str);
+MaritalStatus stringToMaritalStatus(const char *str);
+ResponseCode stringToResponseCode(const char *str);
 
 #endif //UTIL_H
