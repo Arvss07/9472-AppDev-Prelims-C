3b1ac93e59f397b6e449217d91611acd48e1e910
ALFRED CHRISTIAN EMMANUEL NGAOSI <2232837@slu.edu.ph>
2024-09-08 17:04:03 +0800

    case 1 in main switch case

--- menu/menu.c
+++ menu/menu.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "../citizen/citizen.h"
 #include "../file/file.h"
 
 void welcome() {
     printf("Welcome to <name> Application\n");
     List list = {NULL, NULL};
-    loadCitizensFromCSV("..//data.csv", &list);
+    loadCitizensFromCSV("..\\data.csv", &list);
 
     printTable(&list);
 }
 
 
-void printMainMenu() {
+void printMainMenu(List *list) {
     int choice;
     printf("\nMENU:\n");
     printf("1. Add Citizen\n");
@@ -24,10 +25,96 @@ void printMainMenu() {
     printf("6. Create Brgy. Certificate\n");
     printf("7. Demographics\n");
     printf("8. Exit\n");
+    printf("Enter your choice: ");
 
     scanf("%d", &choice);
     switch (choice) {
         case 1:
+            printf("\n You are now adding a citizen");
+
+            char firstName[100];
+            printf("\nWhat is the citizen's first name?: ");
+            scanf("%s", &firstName);
+
+            char middleName[100];
+            printf("\nWhat is the citizen's middle name?: ");
+            scanf("%s", &middleName);
+
+            char lastName[100];
+            printf("\nWhat is the citizen's last name?: ");
+            scanf("%s", &lastName);
+
+            char citizenGender[100];
+            printf("\nWhat is the citizen's gender? <MALE, FEMALE, OTHERS>: ");
+            scanf("%s", &citizenGender);
+            Gender gender;
+            strcpy(gender, citizenGender);
+
+            char birthDate[];
+            printf("\nWhat is the citizen's birth date?: ");
+            scanf("%s", &birthDate);
+
+            char citizenMaritalStatus [100];
+            printf("\nWhat is the citizen's marital status? <SINGLE, MARRIED, DIVORCED, WIDOWED>: ");
+            scanf("%s", &citizenMaritalStatus);
+            MaritalStatus maritalStatus;
+            strcpy(maritalStatus, citizenMaritalStatus);
+
+            char nationality[];
+            printf("\nWhat is the citizen's nationality?: ");
+            scanf("%s", &nationality);
+
+            char religion[];
+            printf("\nWhat is the citizen's religion?: ");
+            scanf("%s", &religion);
+
+            char contactNumber[];
+            printf("\nWhat is the citizen's contact number?: ");
+            scanf("%s", &contactNumber);
+
+            char emailAddress[];
+            printf("\nWhat is the citizen's email address?: ");
+            scanf("%s", &emailAddress);
+
+            char houseNumber[];
+            printf("\nWhat is the citizen's house number?: ");
+            scanf("%s", &houseNumber);
+
+            char street[];
+            printf("\nWhat is the citizen's street?: ");
+            scanf("%s", &street);
+
+            char purokZone[];
+            printf("\nWhat is the citizen's purok zone?: ");
+            scanf("%s", &purokZone);
+
+            Address address;
+            strcpy(address.houseNumber, houseNumber);
+            strcpy(address.street, street);
+            strcpy(address.purokZone, purokZone);
+
+            Citizen newCitizen;
+            strcpy(newCitizen.firstName, firstName);
+            strcpy(newCitizen.middleName, middleName);
+            strcpy(newCitizen.lastName, lastName);
+            strcpy(newCitizen.gender, citizenGender);
+            strcpy(newCitizen.birthDate, birthDate);
+            strcpy(newCitizen.maritalStatus, citizenMaritalStatus);
+            strcpy(newCitizen.nationality, nationality);
+            strcpy(newCitizen.religion, religion);
+            strcpy(newCitizen.contactNumber, contactNumber);
+            strcpy(newCitizen.emailAddress, emailAddress);
+            strcpy(newCitizen.address, address);
+
+            addLast(list, newCitizen);
+            char status[10];
+            strcpy(status, addLast(list, newCitizen));
+
+            if (status == "Success") {
+                printf("\n Citizen successfully added.");
+            } else {
+                printf("\n Citizen not added. Please try again.");
+            }
             break;
         case 2:
             break;
