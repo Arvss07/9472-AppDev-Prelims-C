3df3af85e5044e15768519d4aca79051bd501526
Arvy Aggabao <115993421+Arvss07@users.noreply.github.com>
2024-12-05 04:40:17 +0000

    Added menu

---
+++ menu/menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "../citizen/citizen.h"
+#include "../cmd/terminal_utils.h"
+#include "../utils/util.h"
+
+void welcome(List list);
+void getStringInput(char *destination, const int size, char *prompt, int isUpdate);
+void printMainMenu(List *list);
+void subMenu1();
+#endif
